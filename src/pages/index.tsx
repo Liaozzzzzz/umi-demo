59137a40b197756301f032f0d727962f5438fe2d
liaozzzzzz <996712856@qq.com>
2019-08-01 20:27:08 +0800

    feat: 新增字段

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,10 @@ export default function() {
       <div className={styles.welcome} />
       <ul className={styles.list}>
         <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
+        <h1>拜总牛逼!</h1>
+        <h1>逼神牛逼!</h1>
+        <h1>易神牛逼!</h1>
+        <h1>蛤蟆牛逼!</h1>
         <li>
           <a href="https://umijs.org/guide/getting-started.html">
             Getting Started
